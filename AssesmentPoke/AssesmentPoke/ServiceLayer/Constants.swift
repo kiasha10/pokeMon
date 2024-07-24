aedd5f521c263bde2e882d1aa03be55a15f4c186
kiasha10 <kiasharangasamy2000@gmail.com>
2024-07-24 14:53:42 +0200

    Debugging on Feature HomeScreen

--- AssesmentPoke/AssesmentPoke/ServiceLayer/Constants.swift
+++ AssesmentPoke/AssesmentPoke/ServiceLayer/Constants.swift
@@ -11,9 +11,9 @@ struct Endpoint {
     
     static let pokeMonNames = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100"
     
-    static let pokeMonAbililties = "https://pokeapi.co/api/v2/ability/1/"
-    static let pokeMonGenders = "https://pokeapi.co/api/v2/gender/1/"
-    static let pokeMonSpecies = "https://pokeapi.co/api/v2/pokemon-species/1/"
+//    static let pokeMonAbililties = "https://pokeapi.co/api/v2/ability/1/"
+//    static let pokeMonGenders = "https://pokeapi.co/api/v2/gender/1/"
+//    static let pokeMonSpecies = "https://pokeapi.co/api/v2/pokemon-species/1/"
 }
 
 struct TableViewIdentifiers {
